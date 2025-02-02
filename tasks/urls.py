72b99db9e1b726a675eff0ff5196713d11cb4ee8
10Dhruv10 <145593392+10Dhruv10@users.noreply.github.com>
2025-02-02 23:32:57 +0530

    added option to update

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name=""),
+    path('update-task/<str:pk>/', views.updateTask, name = "update-task"),
+    path('delete-task/<str:pk>/', views.deleteTask, name = "delete-task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -18,3 +18,30 @@ def index(request):
     
     context = {'tasks': tasks, 'TaskForm': form}
     return render(request, 'tasks.html', context)  #we render a template
+
+def updateTask(request, pk):
+    task_object = get_object_or_404(Task, pk = pk)                 #get Model's object (it has all data) with given pk
+    form = TaskForm(instance = task_object)                        #populate form with previous line object (data)
+
+    if request.method == "POST":                                   #if user now submits form after editing -> proceed 
+        form = TaskForm(request.POST, instance = task_object)      #populate form with new data at task_object and not some new object
+        
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')
+    
+    context = {'TaskForm' : form}
+    return render(request, 'update-task.html', context)
+
+def deleteTask(request, pk):
+    task_object = get_object_or_404(Task, pk =  pk)
+    
+    if request.method == 'POST':                                    #post request comes when we click on button to delete
+        task_object.delete()
+        
+        return redirect('/')
+
+    context = {'task' : task_object}
+    
+    return render(request, 'delete-task.html', context)
