d450c8d5eec4dff63b545963dcb157c9a28201d9
Dhruv Sangwan <145593392+10Dhruv10@users.noreply.github.com>
2025-02-02 23:58:55 +0530

    Delete tasks directory

--- tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name=""),
-    path('update-task/<str:pk>/', views.updateTask, name = "update-task"),
-    path('delete-task/<str:pk>/', views.deleteTask, name = "delete-task"),
-]
